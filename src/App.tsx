eb120edacd99c7c8c1330382e33d30234e11c465
Muhammad Rafiansyah <mraansyah34@gmail.com>
2026-04-27 18:14:10 +0700

    feature: adding finder and portfolio components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 
 import { Dock, Navbar, Welcome } from "#components";
-import { Terminal, Safari, Resume } from "#windows";
+import { Terminal, Safari, Resume, Finder } from "#windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -15,6 +15,7 @@ const App = () => {
       <Terminal />
       <Safari />
       <Resume />
+      <Finder />
     </main>
   )
 }
